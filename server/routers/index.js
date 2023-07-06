8fbad9cfb92195b8efe7dbf7b65a56fda1543336
NameIsUser06 <woosoungcho@gmail.com>
2023-07-06 13:14:14 +0900

    FIX : Auth

--- server/routers/index.js
+++ server/routers/index.js
@@ -5,10 +5,12 @@ const imageRouter = require("./image");
 const oauthRouter = require("./oauth");
 const chatRouter = require("./chat");
 const authRouter = require("./auth");
+const diaryRouter = require("./diary");
 
 router.use("/image", imageRouter);
 router.use("/oauth", oauthRouter);
 router.use("/chat", chatRouter);
 router.use("/auth", authRouter);
+router.use("/diary", diaryRouter);
 
 module.exports = router;
