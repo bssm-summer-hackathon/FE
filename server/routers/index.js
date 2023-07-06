6a29b4f787c161dfc44186c66e36f7a55589fc85
NameIsUser06 <woosoungcho@gmail.com>
2023-07-06 13:47:35 +0900

    ADD : Clear

--- server/routers/index.js
+++ server/routers/index.js
@@ -5,12 +5,10 @@ const imageRouter = require("./image");
 const oauthRouter = require("./oauth");
 const chatRouter = require("./chat");
 const authRouter = require("./auth");
-const diaryRouter = require("./diary");
 
 router.use("/image", imageRouter);
 router.use("/oauth", oauthRouter);
 router.use("/chat", chatRouter);
 router.use("/auth", authRouter);
-router.use("/diary", diaryRouter);
 
 module.exports = router;
